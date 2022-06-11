b7b125becb581f930e2f8e9ad779f5ac1847805a
JkDev <jkdevarg@gmail.com>
2022-06-10 23:03:11 -0500

    Django JWT - API

---
+++ app/api/serializers.py
@@ -0,0 +1,7 @@
+from api.models import Task
+from rest_framework import serializers
+
+class TaskSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Task
+        fields = '__all__'
